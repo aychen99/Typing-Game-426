5e940b4ec8e93d90c19cac8a74f0e5e002832f2e
aychen99 <42681629+aychen99@users.noreply.github.com>
2019-12-08 14:34:02 -0500

    Add sample code for HTTP requests to user store

--- scripts/render-logged-in.js
+++ scripts/render-logged-in.js
@@ -11,12 +11,60 @@ function installLogoutButton() {
 
 function installSettingsButton() {
     $('#settings-button').on('click', async function () {
-        let result = await axios({
-            method: 'get',
-            url: url + "/account/status",
-            headers: {'Authorization': 'Bearer ' + localStorage.jwt},
-        });
-        console.log(result);
+        async function getUserSettings() {
+            try {
+                let result = await axios({
+                    method: 'get',
+                    url: url + '/user/settings',
+                    headers: {'Authorization': 'Bearer ' + localStorage.jwt},
+                });
+                return result.data.result;
+            }
+            catch (e) {
+                // If user does not have settings configured,
+                // configure it first
+                await axios({
+                    method: 'post',
+                    url: url + '/user/settings/',
+                    headers: {'Authorization': 'Bearer ' + localStorage.jwt},
+                    data: {
+                        data: {
+                            games_played: '0',
+                            wpm: '0',
+                            theme: 'dark'
+                        }
+                    }
+                });
+                let result = await axios({
+                    method: 'get',
+                    url: url + '/user/settings',
+                    headers: {'Authorization': 'Bearer ' + localStorage.jwt},
+                });
+                return result.data.result;
+            }
+            
+        }
+
+        try {
+            let settings = await getUserSettings();
+            console.log(settings);
+        } catch {
+            $('body').append(`
+                <div class="prompt-background" id="error-prompt-background">
+                    <div class="user-prompt">
+                        <p class="has-text-centered is-size-4" id="uap-header">
+                            Uh oh! Looks like an error occurred. Please try logging 
+                            out and logging back in!
+                        </p>
+                        <br>
+                        <button class="button is-warning" id="close-prompt">Close</button>
+                    </div>
+                </div>
+            `);
+            $('#close-prompt').on('click', function() {
+                $('#error-prompt-background').remove();
+            });
+        }
     });
 }
 
