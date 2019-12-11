076343fdf12987696d04edf72d87725c72200b36
Anh Nguyen <anhog9@yahoo.com>
2019-12-10 20:32:46 -0500

    Improved user signup functionality

--- scripts/render-login.js
+++ scripts/render-login.js
@@ -229,5 +229,4 @@ async function signUserUp(username, password) {
 export default function installButtonsNotLoggedIn() {
   installLoginButton();
   installSignupButton();
-  installLobbyButton();
 }
