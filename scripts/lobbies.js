816108346bf8b7650052d616b260c153049743bc
aychen99 <42681629+aychen99@users.noreply.github.com>
2019-12-12 04:55:36 -0500

    Complete implementation of custom lobbies by users

--- scripts/lobbies.js
+++ scripts/lobbies.js
@@ -110,16 +110,17 @@ function showLobbies(array) {
   for (let i = array.length - 1; i > 3; i--) {
     hold =
       `<div>
-        <span class="has-text-gray"><button class="button is-success is-medium is-fullwidth" id="${array[i].name}">${array[i].name + " | Created By: " + array[i].owner}</button></span>
-      </div>` + hold
+        <span class="has-text-gray"><button class="button is-success is-medium is-fullwidth select-lobby-button" id="${array[i].name}">${array[i].name + " | Created By: " + array[i].owner}</button></span>
+      </div>` + hold;
+    $(`${array[i].name}button`).on('click', handleLobbyClick);
   }
 
   for (let i = 3; i >= 0; i--) {
     hold =
       `<div>
         <span class="has-text-gray"><button class="button is-success is-medium is-fullwidth select-lobby-button" id="${array[i].name}">${array[i].name}</button></span>
-      </div>` + hold
-      $(`${array[i].name}button`).on('click', handleLobbyClick);
+      </div>` + hold;
+    $(`${array[i].name}button`).on('click', handleLobbyClick);
   }
   return hold;
 }
@@ -145,33 +146,134 @@ async function handleLobbyClick(name) {
   await loadTypingGame(lobby.name);
 }
 
-function createALobby() {
+async function createALobby() {
   if (localStorage.jwt) {
+    let textOptions = '';
+    let storedTexts = {};
+    try {
+      let allUsers = (await axios({
+        method: 'get',
+        url: url + '/private/texts',
+        headers: {
+          Authorization: "Bearer " + localStorage.jwt
+        },
+      })).data.result;
+      
+      // Extract all the submitted texts by each user
+      for (let username in allUsers) {
+        let arrayOfSubmittedTexts = allUsers[username];
+        arrayOfSubmittedTexts.forEach(function(item) {
+          let originalTextName = Object.keys(item)[0];
+          let displayedTextName = originalTextName
+                                  + ' | by '
+                                  + username;
+          textOptions += `
+            <option value="${displayedTextName}">${displayedTextName}</option>
+          `;
+          storedTexts[displayedTextName] = item[originalTextName];
+        });
+      }
+    } catch (e) {
+      // Do nothing
+    }
+
+    let selectTextHTML = `
+      <div class="select">
+        <select name="text-options" id="text-options-select">
+          <option value="blank-text">Select or Upload the Text To Use</option>
+          ${textOptions}
+        </select>
+      </div>
+    `;
     $('body').append(`
                   <div class="prompt-background" id="create-mini-prompt-background">
                       <div class="user-prompt">
                         <form>
-                            Name: 
-                            <input type="text" id="lobby-name">
+                            <label class="label">Name: </label>
+                            <input class="input" type="text" id="lobby-name">
                             <br>
 
-                            Passcode:
-                            <input type="text" id="lobby-passcode">
+                            <label class="label">Passcode: </label>
+                            <input class="input" type="text" id="lobby-passcode">
+                            <br>
+
+                            ${selectTextHTML}
+                            <button class="button is-info" id="upload-text">Upload New Text</button>
                             <br>
 
                         </form>
                           <br>
                           <button class="button is-warning" id="close-mini-prompt">Close</button>
                           <button class="button is-info" id="create-mini-prompt">Create</button>
+                        <div id="error-messages"></div>
                       </div>
                   </div>
               `);
+    $('#upload-text').on('click', function(e) {
+      e.preventDefault();
+      $('#create-mini-prompt-background').append(`
+        <div class="user-prompt" id="upload-text-prompt">
+          <input class="input" id="name-of-text" placeholder="Give your text a title here!">
+          <br>
+          <textarea class="textarea"
+                    placeholder="Upload the text you want people to try typing in here!"
+                    id="text-proper"
+              ></textarea>
+          <br>
+          <button class="button" id="upload-text-submit">
+            Submit
+          </button>
+          <button class="button is-warning" id="cancel-text-submit">
+            Cancel
+          </button>
+        </div>
+      `);
+
+      $('#cancel-text-submit').on('click', function() {
+        $('#upload-text-prompt').remove();
+      })
+      $('#upload-text-submit').on('click', async function() {
+        let nameOfText = $('#name-of-text').val();
+        if (nameOfText == '' || $('#text-proper').val() == '') {
+          return;
+        }
+        let data = {};
+        data[nameOfText] = $('#text-proper').val();
+        await axios({
+          method: 'post',
+          url: url + '/private/texts/' 
+                   + localStorage['typing-username'],
+          headers: {
+            Authorization: "Bearer " + localStorage.jwt
+          },
+          data: {
+            data,
+            type: 'merge',
+          },
+        });
+        $('#create-mini-prompt-background').remove();
+        createALobby();
+      });
+    });
     $('#close-mini-prompt').on('click', function (e) {
       $('#create-mini-prompt-background').remove();
       e.preventDefault();
     })
     $('#create-mini-prompt').on('click', async function () {
-      await createLobby();
+      if ($('#lobby-name').val() == '') {
+        $('#error-messages').html('<p>You must give your lobby a name!</p>');
+        return;
+      }
+      
+      let selectedOption = $('#text-options-select option:selected').attr('value');
+      if (selectedOption == 'blank-text') {
+        $('#error-messages').html('<p>You must choose text to use!</p>');
+        return;
+      }
+
+      let textToUse = storedTexts[selectedOption];
+      
+      await createLobby(textToUse);
       $('#create-mini-prompt-background').remove();
       $('#lobbies-prompt-background').remove();
       createLobbyList();
@@ -196,11 +298,11 @@ function createALobby() {
 
 }
 
-async function createLobby() {
+async function createLobby(textToUse) {
   let hold = $("#lobby-name").val();
   let hasP = false;
 
-  if (!($("#lobby-passcode") == "")) {
+  if (!($("#lobby-passcode").val() == "")) {
     hasP = true;
   }
 
@@ -211,6 +313,7 @@ async function createLobby() {
       hasPasscode: hasP,
       passcode: $("#lobby-passcode").val(),
       owner: localStorage['typing-username'],
+      text: textToUse,
     }
   })
     .catch(error => {
